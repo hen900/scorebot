04a14325a4ba0bfa8a6b2cdb9c0d087f80a2c46b
hdad43 <hmm.mackay@gmail.com>
2019-04-15 21:05:12 -0400

    Update install.sh

--- scoring/install.sh
+++ scoring/install.sh
@@ -3,8 +3,7 @@ mkdir /usr/bin/scorebot
 cp scorebot /etc/init.d/
 update-rc.d scorebot defaults
 apt update 
-apt install  python-pip
-pip install cython==0.25
+apt install cython
 cython scorebot.py --embed
 gcc  -I/usr/include/python2.7/ scorebot.c -lpython2.7 -o /usr/bin/scorebot/scorebot
 
