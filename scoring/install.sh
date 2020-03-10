e5fd54bb0c6656d10ee339221022f6870b2005ee
hdad43 <hmm.mackay@gmail.com>
2020-03-10 07:35:35 -0400

    Update install.sh

--- scoring/install.sh
+++ scoring/install.sh
@@ -3,6 +3,7 @@ mkdir /usr/bin/scorebot
 cp scorebot /etc/init.d/
 update-rc.d scorebot defaults
 apt update 
+#Note: cython does not completely obfuscate scoring booleans
 apt install cython
 cython scorebot.py --embed
 gcc  -I/usr/include/python2.7/ scorebot.c -lpython2.7 -o /usr/bin/scorebot/scorebot
